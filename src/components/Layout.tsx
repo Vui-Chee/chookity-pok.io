3719683a244e13d1b5eda02f2d8b6b6008b19624
Vui-Chee <vuicheesiew@gmail.com>
2021-08-16 15:19:38 +0800

    Test changing colors directly from styled-comp

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,28 +11,34 @@ import SEO from "./Seo"
 
 import { useThemeUI } from "theme-ui"
 
+const theme = {
+  color: "yellow",
+  background: "green",
+}
+
 const Layout: React.FC<{
   title: string
   socialImage?: string
   description?: string
 }> = ({ children, title, description = "", socialImage = "" }) => {
   const context = useThemeUI()
-  React.useEffect(() => {
-    // testing only
-    console.log("on mount", context)
-  }, [])
 
   return (
     <Fragment>
       <GlobalStyle />
       <SEO title={title} description={description} socialImage={socialImage} />
       <LayoutWrapper
-        sx={(theme) => {
-          console.log(theme.rawColors)
-          return {
-            ...theme.rawColors,
-          }
-        }}
+        theme={
+          context.colorMode === "dark"
+            ? {
+                color: "white",
+                background: "black",
+              }
+            : {
+                color: "black",
+                background: "white",
+              }
+        }
       >
         <Header />
         <main>
@@ -47,6 +53,9 @@ const Layout: React.FC<{
 export default Layout
 
 const LayoutWrapper = styled.div`
+  color: ${(props) => props.theme.color};
+  background-color: ${(props) => props.theme.background};
+
   min-height: 100vh;
   display: flex;
   flex-direction: column;
